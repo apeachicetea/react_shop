9c09f7256e162ece6cd6d55ea26ce3e11c8da8eb
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-10-23 15:45:53 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import Data from './data';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail'
+
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link><Link to="/">Home</Link></Nav.Link>
+          <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      
+      <Switch>
+
+        <Route exact path="/">
+          <div className="jumbotron">
+            <h1>20% Season OFF</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident maiores doloribus, quos similique perferendis fugit et accusantium,
+              <br/>iusto dolor labore suscipit laboriosam ullam unde eum voluptatum debitis, rem voluptate ipsa.
+            </p>
+            <p>
+            <Button variant="primary">Learn more</Button>{' '}
+            </p>
+          </div>
+          <div className="container">
+            <div className="row">
+            
+              {
+                shoes.map((el, i)=>{
+                  return <Card shoes = { shoes[i] } i = { i } key = { i } />
+                })
+              }
+
+              {/* <Card shoes = { shoes[0] } />
+              <Card shoes = { shoes[1] } />
+              <Card shoes = { shoes[2] } /> */}
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail/:id">
+          <Detail shoes = { shoes } />
+        </Route>
+        
+      </Switch>
     </div>
   );
 }
+
+
+
+
+function Card(props) {
+
+  return (
+    <div className="col-md-4" key={props.key}>
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg' } width="100%" />
+      <h4> { props.shoes.title } </h4>
+      <p>{ props.shoes.content } & { props.shoes.price }</p>
+    </div>
+  )
+
+}
+
+
 
 export default App;
